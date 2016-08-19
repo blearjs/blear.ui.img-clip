faae1482f3b1c6e7f1718e62ab9847bc351d2544
#云淡然 <cloudcome@163.com>
2016-08-19 17:20:49 +0800

    + blear.ui.img-clip

--- example/index.js
+++ example/index.js
@@ -10,8 +10,24 @@
 
 var ImgClip = require('../src/index');
 
-window.ic = new ImgClip({
-    el: '#demo'
+var retEl = document.getElementById('ret');
+var ret2El = document.getElementById('ret2');
+var changeImageEl = document.getElementById('changeImage');
+
+var ic = window.ic = new ImgClip({
+    el: '#demo',
+    ratio: 1
 });
 
+ic.on('change', function (sel) {
+    retEl.innerHTML = JSON.stringify(sel, null, 4);
+    ret2El.style.width = sel.srcWidth + 'px';
+    ret2El.style.height = sel.srcHeight + 'px';
+    ret2El.style.backgroundPosition = '-' + sel.srcLeft + 'px -' + sel.srcTop + 'px';
+    ret2El.style.backgroundImage = 'url(' + ic.getImage() + ')';
+});
 
+changeImageEl.onclick = function () {
+    changeImageEl.disabled = true;
+    ic.changeImage('http://att.bbs.duowan.com/forum/201608/19/012109bz44gbng5fpw94mp.jpg');
+};
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ var object = require('blear.utils.object');
 var selector = require('blear.core.selector');
 var attribute = require('blear.core.attribute');
 var layout = require('blear.core.layout');
+var event = require('blear.core.event');
 var modification = require('blear.core.modification');
 var Resizable = require('blear.classes.resizable');
 var Draggable = require('blear.classes.draggable');
@@ -20,7 +21,7 @@ var template = require('./template.html');
 var style = require('./style.css');
 var gif = require('./line.gif', 'file');
 
-
+var win = window;
 var namespace = 'blearui-imgClip';
 var defaults = {
     /**
@@ -36,7 +37,7 @@ var defaults = {
     /**
      * 裁剪比例
      */
-    ratio: 1,
+    ratio: 0,
 
     /**
      * 裁剪宽度
@@ -66,7 +67,12 @@ var defaults = {
     /**
      * 裁剪最大高度
      */
-    maxHeight: 200
+    maxHeight: 200,
+
+    /**
+     * 开始选区的最小值
+     */
+    minSelectionSize: 10
 };
 var ImgClip = UI.extend({
     className: 'ImgClip',
@@ -79,95 +85,299 @@ var ImgClip = UI.extend({
     },
 
 
-    changeImg: function () {
+    /**
+     * 改变当前裁剪图片的地址
+     * @param url
+     * @returns {ImgClip}
+     */
+    changeImage: function (url) {
+        var the = this;
 
+        the[_changeImage](url);
+
+        return the;
+    },
+
+
+    /**
+     * 获取当前裁剪的图片地址
+     * @returns {string}
+     */
+    getImage: function () {
+        return this[_imgEl].src;
+    },
+
+    getSelection: function () {
+
+    },
+
+    setSelection: function () {
+
+    },
+
+    release: function () {
+        var the = this;
+
+        the[_changeMode](false);
+        the[_selectionLeftTopWidthHeight] = [0, 0, 0, 0];
+
+        return the;
     }
 });
 var _options = ImgClip.sole();
 var _initNode = ImgClip.sole();
 var _initEvent = ImgClip.sole();
+var _changeImage = ImgClip.sole();
+var _changeMode = ImgClip.sole();
+var _changeSelection = ImgClip.sole();
+var _parseSelection = ImgClip.sole();
 var _imgEl = ImgClip.sole();
+var _containerEl = ImgClip.sole();
 var _cloneEl = ImgClip.sole();
 var _showEl = ImgClip.sole();
 var _trackerEl = ImgClip.sole();
 var _resizerEl = ImgClip.sole();
 var _resizable = ImgClip.sole();
-var _draggable = ImgClip.sole();
+var _resizerDraggable = ImgClip.sole();
+var _trackerDraggable = ImgClip.sole();
+var _imgDisplaySizes = ImgClip.sole();
+var _imgOriginalSizes = ImgClip.sole();
+var _selectionLeftTopWidthHeight = ImgClip.sole();
+var _showPosition = ImgClip.sole();
 var pro = ImgClip.prototype;
 
 pro[_initNode] = function () {
     var the = this;
     var options = the[_options];
     var imgEl = the[_imgEl] = selector.query(options.el)[0];
-    var imgWidth = layout.outerWidth(imgEl);
-    var imgHeight = layout.outerHeight(imgEl);
-    var containerEl = modification.parse(template);
-    var cloneEl = the[_cloneEl] = selector.query('.' + namespace + '-clone', containerEl)[0];
-    var showEl = the[_showEl] = selector.query('.' + namespace + '-show', containerEl)[0];
-    var trackerEl = the[_trackerEl] = selector.query('.' + namespace + '-tracker', containerEl)[0];
-    var resizerEl = the[_resizerEl] = selector.query('.' + namespace + '-resizer', containerEl)[0];
+    var containerEl = the[_containerEl] = modification.parse(template);
 
-    attribute.style(containerEl, {
-        width: imgWidth,
-        height: imgHeight
-    });
+    the[_cloneEl] = selector.query('.' + namespace + '-clone', containerEl)[0];
+    the[_showEl] = selector.query('.' + namespace + '-show', containerEl)[0];
+    the[_trackerEl] = selector.query('.' + namespace + '-tracker', containerEl)[0];
+    the[_resizerEl] = selector.query('.' + namespace + '-resizer', containerEl)[0];
+    the[_changeImage](imgEl.src);
+};
 
-    loader.img(imgEl.src, function (err, _imgEl) {
+
+pro[_changeImage] = function (url) {
+    var the = this;
+    var options = the[_options];
+
+    the.emit('beforeLoading');
+    attribute.show(the[_imgEl]);
+    attribute.hide(the[_containerEl]);
+    loader.img(url, function (err, originalImg) {
+        the.emit('afterLoading');
+
         if (err) {
-            throw new Error('图片加载失败：' + imgEl.src);
+            return the.emit('error', err);
         }
 
-        attribute.hide(imgEl);
-        modification.insert(containerEl, imgEl, 'afterend');
-        cloneEl.src = showEl.src = _imgEl.src;
-        the[_resizable] = new Resizable({
-            el: resizerEl,
-            width: options.width,
-            height: options.height,
-            minWidth: options.minWidth,
-            minHeight: options.minHeight,
-            maxWidth: options.maxWidth,
-            maxHeight: options.maxHeight,
-            ratio: options.ratio
+        the.release();
+        the[_imgEl].src = the[_cloneEl].src = the[_showEl].src = originalImg.src;
+
+        var imgWidth = layout.outerWidth(the[_imgEl]);
+        var imgHeight = layout.outerHeight(the[_imgEl]);
+
+        attribute.hide(the[_imgEl]);
+        attribute.show(the[_containerEl]);
+        the[_showPosition] = the[_selectionLeftTopWidthHeight] = [0, 0, 0, 0];
+        the[_imgDisplaySizes] = [imgWidth, imgHeight];
+        the[_imgOriginalSizes] = [originalImg.width, originalImg.height];
+        the.emit('change', the[_parseSelection]());
+
+        attribute.style(the[_containerEl], {
+            width: imgWidth,
+            height: imgHeight
         });
-        the[_draggable] = new Draggable({
-            containerEl: resizerEl,
-            effectedSelector: resizerEl,
-            handleSelector: resizerEl,
-            shadow: false
+        attribute.style(the[_cloneEl], {
+            width: imgWidth,
+            height: imgHeight
         });
-        the[_initEvent]();
+        attribute.style(the[_showEl], {
+            width: imgWidth,
+            height: imgHeight
+        });
+
+        if (!the[_resizable]) {
+            modification.insert(the[_containerEl], the[_imgEl], 'afterend');
+            the[_resizable] = new Resizable({
+                el: the[_resizerEl],
+                width: options.width,
+                height: options.height,
+                minWidth: options.minWidth,
+                minHeight: options.minHeight,
+                maxWidth: options.maxWidth,
+                maxHeight: options.maxHeight,
+                ratio: options.ratio,
+                resizable: false
+            });
+            the[_resizerDraggable] = new Draggable({
+                containerEl: the[_resizerEl],
+                effectedSelector: the[_resizerEl],
+                handleSelector: the[_resizerEl],
+                shadow: false,
+                draggable: false
+            });
+            the[_trackerDraggable] = new Draggable({
+                containerEl: the[_trackerEl],
+                effectedSelector: the[_trackerEl],
+                handleSelector: the[_trackerEl],
+                shadow: false,
+                draggable: false
+            });
+            the[_initEvent]();
+        }
     });
 };
 
 pro[_initEvent] = function () {
     var the = this;
+    var options = the[_options];
 
+    // 尺寸区尺寸改变
     the[_resizable].on('resizeStart', function () {
-        the[_draggable].disable();
+        the[_resizerDraggable].disable();
     });
 
-    the[_resizable].on('resizeMove', function () {
-
+    the[_resizable].on('resizeMove', function (meta) {
+        the[_changeSelection](null, [meta.width, meta.height]);
     });
 
-    the[_resizable].on('resizeStop', function () {
-        the[_draggable].enable();
+    the[_resizable].on('resizeEnd', function () {
+        the[_resizerDraggable].enable();
     });
 
-    the[_draggable].on('dragStart', function () {
+    // 背景拖拽
+    the[_trackerDraggable].on('dragStart', function (meta) {
         the[_resizable].disable();
+        the[_resizerDraggable].disable();
+        the[_selectionLeftTopWidthHeight][2] = the[_selectionLeftTopWidthHeight][3] = 0;
+        the[_changeMode](true);
+
+        var scrollTop = layout.scrollTop(win);
+        var scrollLeft = layout.scrollLeft(win);
+        var containerTop = layout.offsetTop(the[_containerEl]) - scrollTop;
+        var containerLeft = layout.offsetLeft(the[_containerEl]) - scrollLeft;
+        var styleLeft = meta.startX - containerLeft;
+        var styleTop = meta.startY - containerTop;
+
+        the[_changeSelection]([styleLeft, styleTop]);
     });
 
-    the[_draggable].on('dragMove', function () {
-
+    the[_trackerDraggable].on('dragMove', function (meta) {
+        the[_changeSelection](null, [meta.deltaX, meta.deltaY]);
     });
 
-    the[_draggable].on('dragStop', function () {
+    the[_trackerDraggable].on('dragEnd', function (meta) {
+        var minSize = the[_resizable].getMinSize();
+        var minSelectionSize = options.minSelectionSize;
+
+        if (meta.deltaX < minSelectionSize && meta.deltaY < minSelectionSize) {
+            the[_changeMode](false);
+        } else {
+            the[_resizable].enable();
+            the[_resizerDraggable].enable();
+
+            // 不足最小尺寸
+            if (meta.deltaX < minSize.width || meta.deltaY < minSize.height) {
+                the[_changeSelection](null, [minSize.width, minSize.height]);
+            }
+        }
+    });
+
+    // 尺寸区拖拽
+    var selectionLeft = 0;
+    var selectionTop = 0;
+    the[_resizerDraggable].on('dragStart', function () {
+        the[_resizable].disable();
+        selectionLeft = the[_selectionLeftTopWidthHeight][0];
+        selectionTop = the[_selectionLeftTopWidthHeight][1];
+    });
+
+    the[_resizerDraggable].on('dragMove', function (meta) {
+        attribute.style(the[_showEl], {
+            left: the[_showPosition][0] - meta.deltaX,
+            top: the[_showPosition][1] - meta.deltaY
+        });
+        the[_changeSelection]([
+            selectionLeft + meta.deltaX,
+            selectionTop + meta.deltaY
+        ]);
+    });
+
+    the[_resizerDraggable].on('dragEnd', function (meta) {
         the[_resizable].enable();
+        the[_showPosition][0] -= meta.deltaX;
+        the[_showPosition][1] -= meta.deltaY;
+        the[_selectionLeftTopWidthHeight][0] = selectionLeft + meta.deltaX;
+        the[_selectionLeftTopWidthHeight][1] = selectionTop + meta.deltaY;
     });
 };
 
+pro[_changeMode] = function (isSelecting) {
+    var the = this;
+
+    attribute.style(the[_cloneEl], 'opacity', isSelecting ? 0.6 : 1);
+    attribute.style(the[_resizerEl], 'display', isSelecting ? 'block' : 'none');
+};
+
+pro[_changeSelection] = function (positions, sizes) {
+    var the = this;
+    var left = positions ? positions[0] : the[_selectionLeftTopWidthHeight][0];
+    var top = positions ? positions[1] : the[_selectionLeftTopWidthHeight][1];
+    var width = sizes ? sizes[0] : the[_selectionLeftTopWidthHeight][2];
+    var height = sizes ? sizes[1] : the[_selectionLeftTopWidthHeight][3];
+    // var minSize = the[_resizable].getMinSize();
+    var maxSize = the[_resizable].getMaxSize();
+    var maxWidth = maxSize.width;
+    var maxHeight = maxSize.height;
+
+    left = Math.max(left, 0);
+    top = Math.max(top, 0);
+    width = Math.min(width, maxWidth);
+    height = Math.min(height, maxHeight);
+
+    if (left + width > the[_imgDisplaySizes][0]) {
+        left = the[_imgDisplaySizes][0] - width;
+    }
+
+    if (top + height > the[_imgDisplaySizes][1]) {
+        top = the[_imgDisplaySizes][1] - height;
+    }
+
+    the[_selectionLeftTopWidthHeight] = [left, top, width, height];
+    the.emit('change', the[_parseSelection]());
+
+    attribute.style(the[_resizerEl], {
+        left: left,
+        top: top,
+        width: width,
+        height: height
+    });
+    attribute.style(the[_showEl], {
+        left: the[_showPosition][0] = -left,
+        top: the[_showPosition][1] = -top
+    });
+};
+
+pro[_parseSelection] = function () {
+    var the = this;
+    var widthRatio = the[_imgDisplaySizes][0] / the[_imgOriginalSizes][0];
+    var heightRatio = the[_imgDisplaySizes][1] / the[_imgOriginalSizes][1];
+
+    return {
+        left: the[_selectionLeftTopWidthHeight][0],
+        top: the[_selectionLeftTopWidthHeight][1],
+        width: the[_selectionLeftTopWidthHeight][2],
+        height: the[_selectionLeftTopWidthHeight][3],
+        srcLeft: the[_selectionLeftTopWidthHeight][0] / widthRatio,
+        srcTop: the[_selectionLeftTopWidthHeight][1] / heightRatio,
+        srcWidth: the[_selectionLeftTopWidthHeight][2] / widthRatio,
+        srcHeight: the[_selectionLeftTopWidthHeight][3] / heightRatio
+    };
+};
 
 style += '.' + namespace + '-line{' +
     /**/'background:#fff url("' + gif + '");' +
