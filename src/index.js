3dd6c025b3238d97c8f5bb9e720ad7ed5ad75653
#云淡然 <cloudcome@163.com>
2016-08-19 14:25:18 +0800

    + blear.ui.img-clip

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ var selector = require('blear.core.selector');
 var attribute = require('blear.core.attribute');
 var layout = require('blear.core.layout');
 var modification = require('blear.core.modification');
+var Resizable = require('blear.classes.resizable');
+var Draggable = require('blear.classes.draggable');
 
 var template = require('./template.html');
 var style = require('./style.css');
@@ -83,7 +85,14 @@ var ImgClip = UI.extend({
 });
 var _options = ImgClip.sole();
 var _initNode = ImgClip.sole();
+var _initEvent = ImgClip.sole();
 var _imgEl = ImgClip.sole();
+var _cloneEl = ImgClip.sole();
+var _showEl = ImgClip.sole();
+var _trackerEl = ImgClip.sole();
+var _resizerEl = ImgClip.sole();
+var _resizable = ImgClip.sole();
+var _draggable = ImgClip.sole();
 var pro = ImgClip.prototype;
 
 pro[_initNode] = function () {
@@ -93,8 +102,10 @@ pro[_initNode] = function () {
     var imgWidth = layout.outerWidth(imgEl);
     var imgHeight = layout.outerHeight(imgEl);
     var containerEl = modification.parse(template);
-    var cloneEl = selector.query('.' + namespace + '-clone', containerEl)[0];
-    var showEl = selector.query('.' + namespace + '-show', containerEl)[0];
+    var cloneEl = the[_cloneEl] = selector.query('.' + namespace + '-clone', containerEl)[0];
+    var showEl = the[_showEl] = selector.query('.' + namespace + '-show', containerEl)[0];
+    var trackerEl = the[_trackerEl] = selector.query('.' + namespace + '-tracker', containerEl)[0];
+    var resizerEl = the[_resizerEl] = selector.query('.' + namespace + '-resizer', containerEl)[0];
 
     attribute.style(containerEl, {
         width: imgWidth,
@@ -109,6 +120,51 @@ pro[_initNode] = function () {
         attribute.hide(imgEl);
         modification.insert(containerEl, imgEl, 'afterend');
         cloneEl.src = showEl.src = _imgEl.src;
+        the[_resizable] = new Resizable({
+            el: resizerEl,
+            width: options.width,
+            height: options.height,
+            minWidth: options.minWidth,
+            minHeight: options.minHeight,
+            maxWidth: options.maxWidth,
+            maxHeight: options.maxHeight,
+            ratio: options.ratio
+        });
+        the[_draggable] = new Draggable({
+            containerEl: resizerEl,
+            effectedSelector: resizerEl,
+            handleSelector: resizerEl,
+            shadow: false
+        });
+        the[_initEvent]();
+    });
+};
+
+pro[_initEvent] = function () {
+    var the = this;
+
+    the[_resizable].on('resizeStart', function () {
+        the[_draggable].disable();
+    });
+
+    the[_resizable].on('resizeMove', function () {
+
+    });
+
+    the[_resizable].on('resizeStop', function () {
+        the[_draggable].enable();
+    });
+
+    the[_draggable].on('dragStart', function () {
+        the[_resizable].disable();
+    });
+
+    the[_draggable].on('dragMove', function () {
+
+    });
+
+    the[_draggable].on('dragStop', function () {
+        the[_resizable].enable();
     });
 };
 
